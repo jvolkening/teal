0067d3c358302ef0dc779861233f75dc0ebfb1da
jvolkening <jdv@base2bio.com>
2026-07-10 20:05:19 -0500

    more tweaks to buttons for compactness

--- client/src/static/js/traceView.js
+++ client/src/static/js/traceView.js
@@ -58,14 +58,14 @@ function resetGlobalValues() {
 
 function createButtons() {
     var html = '<div id="traceView-Buttons" class="d-none">';
-    html += '  <button id="traceView-nav-bw-win" class="btn btn-outline-secondary">prev</button>';
+    html += '  <button id="traceView-nav-bw-win" class="btn btn-outline-secondary">&lt;&lt;</button>';
     html += '  <button id="traceView-nav-bw-bit" class="btn btn-outline-secondary">&lt;</button>';
-    html += '  <button id="traceView-nav-zy-in" class="btn btn-outline-secondary">Zoom In Y</button>';
-    html += '  <button id="traceView-nav-zy-out" class="btn btn-outline-secondary">Zoom Out Y</button>';
-    html += '  <button id="traceView-nav-zx-in" class="btn btn-outline-secondary">Zoom In X</button>';
-    html += '  <button id="traceView-nav-zx-out" class="btn btn-outline-secondary">Zoom Out X</button>';
+    html += '  <button id="traceView-nav-zx-in" class="btn btn-outline-secondary"><span class="fa-solid fa-magnifying-glass-plus"></span> X</button>';
+    html += '  <button id="traceView-nav-zx-out" class="btn btn-outline-secondary"><span class="fa-solid fa-magnifying-glass-minus"></span> X</button>';
+    html += '  <button id="traceView-nav-zy-in" class="btn btn-outline-secondary"><span class="fa-solid fa-magnifying-glass-plus"></span> Y</button>';
+    html += '  <button id="traceView-nav-zy-out" class="btn btn-outline-secondary"><span class="fa-solid fa-magnifying-glass-minus"></span> Y</button>';
     html += '  <button id="traceView-nav-fw-bit" class="btn btn-outline-secondary">&gt;</button>';
-    html += '  <button id="traceView-nav-fw-win" class="btn btn-outline-secondary">next</button>';
+    html += '  <button id="traceView-nav-fw-win" class="btn btn-outline-secondary">&gt;&gt;</button>';
     html += '  <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>';
     html += '  <button id="traceView-nav-hi-a" class="btn btn-outline-secondary"><strong>A</strong></button>';
     html += '  <button id="traceView-nav-hi-c" class="btn btn-outline-secondary"><strong>C</strong></button>';
